0780d6d5b7cc8795c8b86f796292a1a69d8143e5
ZhangHongZuo <zhanghongzuo@yunfan.com>
2016-12-09 18:44:56 +0800

    add-增加使用layout_viewpager实现的方式

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,35 +1,62 @@
 package com.example.administrator.myapplication;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import Reclye.SeriesTableFragment;
+import Reclye.SeriesSelectorTableAdapter;
 import vp_select.Selector;
 
 public class MainActivity extends Activity {
+    List<Integer> tempList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempList = new ArrayList<>();
+        for (int i = 0; i < 706; i++) {
+            tempList.add(i);
+        }
+        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
     }
 
     public void go(View view) {
         Selector selector = new Selector();
         List<View> list = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++){
-            TextView tv = new TextView(this);
-            tv.setText("tab-"+i);
-            list.add(tv);
+        List<List<Integer>> fu = utils.cal(tempList, 50);
+        for (int i = 0; i < fu.size(); i++) {
+            RecyclerView recyclerView = new RecyclerView(this);
+            recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+                @Override
+                public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                    super.getItemOffsets(outRect, view, parent, state);
+
+                    outRect.top = 5;
+                    outRect.left = 5;
+                    outRect.right = 5;
+                    outRect.bottom = 5;
+
+                }
+            });
+            GridLayoutManager manager = new GridLayoutManager(this, 5);
+            recyclerView.setLayoutManager(manager);
+            SeriesSelectorTableAdapter adapter = new SeriesSelectorTableAdapter();
+            adapter.setDataList(fu.get(i));
+            recyclerView.setAdapter(adapter);
+            list.add(recyclerView);
         }
+
         selector.setList(list);
         android.app.FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
---
+++ app/src/main/java/com/example/administrator/myapplication/Utils.java
@@ -0,0 +1,51 @@
+package com.example.administrator.myapplication;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by FTE on 2016/12/9.
+ */
+
+public class utils {
+
+
+    /**
+     * 将集合按照指定区块大小分隔
+     *
+     * @param list      待分解集合
+     * @param blockArgs 区块大小
+     * @return 分隔好的集合
+     */
+    public static List<List<Integer>> cal(List<Integer> list, int blockArgs) {
+
+        int blockSize = blockArgs;
+        List<List<Integer>> result = new ArrayList<>();
+        int TotalCount = list.size();
+        if (TotalCount <= 50) {
+            return result;
+        }
+
+        int BlockNum = TotalCount / blockSize;
+        int LastBlockContentCount = TotalCount % blockSize;
+
+        Log.e("ZHZ", "总数 ： " + TotalCount + " block 数量: " + BlockNum + " 最后一个block size : " + LastBlockContentCount);
+
+        int startIndex = 0;
+
+        for (int i = 0; i < BlockNum; i++) {
+            startIndex = i * blockSize;
+            List<Integer> temp = list.subList(startIndex, startIndex + blockSize);
+            result.add(temp);
+        }
+
+        if (LastBlockContentCount > 0) {
+            List<Integer> temp3 = list.subList(startIndex + blockSize, TotalCount - 1);
+            result.add(temp3);
+        }
+
+        return result;
+    }
+}
